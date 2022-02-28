88b8c16ab806de377c344bb4fb6487efda4ce4d4
bnfnal <59770118+bnfnal@users.noreply.github.com>
2022-02-28 12:07:29 +0200

    Update Main.java
    добавила новую задачу

--- Main.java
+++ Main.java
@@ -3,15 +3,31 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         
+        Scanner sc = new Scanner(System.in);
+        
         System.out.println("Задача №1: Even or Odd");
         // Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
         // Function EvenOrOdd(int x)
         
         System.out.println("Введите целое число");
-        Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         System.out.println("Ответ: " + EvenOrOdd(n));
+        System.out.println();
         
+        
+        System.out.println("Задача №2: Sum of positive");
+        // You get an array of numbers, return the sum of all of the positives ones.
+        // Function SumOfPositive(int[] array)
+        
+        System.out.println("Введите количество элементов в массиве");
+        int m = sc.nextInt();
+        int[] array = new int[m];
+        System.out.println("Введите элементы массива (целые числа)");
+        for (int i = 0; i<m; i++){
+            array[i] = sc.nextInt();
+        }
+        System.out.println("Ответ: " + SumOfPositive(array));
+        System.out.println();
     }
     
     public static String EvenOrOdd(int x)
@@ -24,6 +40,15 @@ public class Main {
         {
             return("Odd");
         }
+    }
+    
+    public static int SumOfPositive(int[] array)
+    {
+        int sum = 0;
+        for (int i = 0; i<array.length; i++){
+            if (array[i] > 0) sum += array[i];
+        }
+        return sum;
     }
 
 }
